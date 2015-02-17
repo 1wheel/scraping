86f441ec5ee11b9d9023f0c74624cd807ff6e32f
Adam Pearce <1wheel@gmail.com>
2015-02-16 20:34:29 -0500

    graphs, mm

---
+++ oscars/js/nominations.js
@@ -0,0 +1,76 @@
+loadData(function(data){
+  console.log(data.length)
+  // debugger
+
+
+
+  var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+  var nominations = _.filter(data, {award: "ACTOR"})
+
+  var x = d3.scale.linear().domain(d3.extent(nominations, f('nth')))    .range([0, width])
+  var y = d3.scale.linear().domain(d3.extent(nominations, f('numPrev'))).range([height, 0])
+
+  nominations = _.sortBy(nominations, 'won')
+
+  d3.nest().key(function(d){ return d.nth + ' - ' +  d.numPrev }).entries(nominations).forEach(function(year){
+    _.sortBy(year.values, 'won').forEach(function(d, i){
+      d.offset = i*2
+      d.xPos = x(d.nth)     + d.offset/2
+      d.yPos = y(d.numPrev) - d.offset
+      if (d.won) year.winPrev = d.numPrev
+    })
+
+    year.avgPrev = d3.mean(year.values, f('numPrev'))
+  })
+
+
+
+  var career = svg.append('path.career')
+
+  svg.selectAll('circle')
+      .data(nominations).enter()
+    .append('circle.nom').classed('won', f('won'))
+      .attr('r', 8)
+      .attr('cx', f('xPos'))
+      .attr('cy', f('yPos'))
+      .call(tooltip)
+      .on('mouseover', function(d){
+        var allNom = _.sortBy(_.filter(nominations, {name: d.name}), 'nth')
+
+        career.attr('d', d3.svg.line().x(f('xPos')).y(f('yPos'))(allNom))
+      })
+
+
+
+  var line = d3.svg.line().x(_.compose(x, f('key'))).y(_.compose(y, f('avgPrev')))
+
+  var byYear = d3.nest().key(f('nth')).entries(nominations)
+  byYear.forEach(function(year){
+    year.prevWin = _.findWhere(year.values, {won: true}).numPrev
+    year.prevNom = d3.mean(year.values, f('numPrev'))
+  })
+
+
+  var smoothing = 15
+  byYear.forEach(function(d, i){
+    var smoothYears = byYear.slice(Math.max(0, i - smoothing), i + 1)
+    d.smoothWin = d3.mean(smoothYears, f('prevWin'))
+    d.smoothNom = d3.mean(smoothYears, f('prevNom'))
+  })
+
+
+  svg.selectAll('agg-path')
+      .data(['prevWin', 'prevNom', 'smoothWin', 'smoothNom']).enter()
+    .append('path')
+      .attr('d', function(d){
+        return line.y(_.compose(y, f(d)))(byYear) })
+      .attr('class', f())
+      .classed('agg-path', true)
+
+})
---
+++ oscars/js/shared.js
@@ -0,0 +1,37 @@
+var idToObjects = {}
+var globalData
+
+function loadData(cb){
+  d3.csv('data.csv', function(data){
+    globalData = data
+    data.forEach(function(d){
+      var str = d.year.split('(')[1].split(')')[0]
+      d.nth = +str.slice(0, str.length - 2)
+
+      d.won = d.won == 'true' ? true : false
+
+      d.id = d.award + '-' + d.name
+
+      if (!idToObjects[d.id]) idToObjects[d.id] = []
+      d.prevNominations = idToObjects[d.id].slice()
+      d.numPrev = d.prevNominations.length
+      idToObjects[d.id].push(d)
+    })
+
+    cb(data)
+  })
+}
+
+
+var margin = {top: 40, right: 20, bottom: 40, left: 20},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+
+function f(str){ return function(obj){ return str ? obj[str] : obj }}
+
+
+
+function tooltip(selection){
+  selection.on('click', function(d){ console.log(d) })
+}
