39491c96dbe0e7e49370218e3759ad5dcadc0bc6
Adam Pearce <1wheel@gmail.com>
2015-03-23 13:32:02 -0400

    results and standings

---
+++ nba/downloadResults.js
@@ -0,0 +1,20 @@
+var request = require('request')
+var d3 = require('d3')
+var queue = require('queue-async')
+var fs = require('fs')
+
+var q = queue(5)
+
+d3.range(1951, 2015).forEach(function(year){
+	q.defer(function(cb){
+		var url = 'http://www.basketball-reference.com/leagues/NBA_'
+		+ year + '_games.html'
+		request(url, function(error, response, html){
+			console.log(year)
+			fs.writeFile(__dirname + '/resultsRaw/' + year + '.html',
+						html, 
+						function(){})
+			cb()
+		})
+	})
+})
---
+++ nba/downloadStandings.js
@@ -0,0 +1,20 @@
+var request = require('request')
+var d3 = require('d3')
+var queue = require('queue-async')
+var fs = require('fs')
+
+var q = queue(5)
+
+d3.range(1951, 2015).forEach(function(year){
+	q.defer(function(cb){
+		var url = 'http://www.basketball-reference.com/leagues/NBA_'
+		+ year + '_standings.html'
+		request(url, function(error, response, html){
+			console.log(year)
+			fs.writeFile(__dirname + '/standingsRaw/' + year + '.html',
+						html, 
+						function(){})
+			cb()
+		})
+	})
+})
