9ad48516a1b538e4c80c727bf75f4635ded08b88
Adam Pearce <1wheel@gmail.com>
2016-01-20 22:52:34 -0500

    first pass at parsing

--- all-nba-games/parse.js
+++ all-nba-games/parse.js
@@ -7,41 +7,42 @@ var request = require('request')
 var _ = require('lodash')
 var queue = require('queue-async')
 
-var q = queue(5)
-
-var downloaded = glob.sync(__dirname + '/raw-box/*.json').map(pathToID)
-
-d3.range(1, 1231)
-		.map(d => '002140' + d3.format('04d')(d))
-		.filter(d =>  !_.contains(downloaded, d))
-		.forEach(d => q.defer(downloadBox, d))
 
 
 
-function pathToID(d){ return _.last(d.split('/')).replace('.json', '') }
+var games = glob.sync(__dirname + '/raw-box/*.json')
+  .map(function(fileStr,i){
+    var res = JSON.parse(fs.readFileSync(fileStr, 'utf-8')) 
+    if (res.message) return null
+
+    var rs = res.resultSets[5].rowSet
+    var rv = {}
+    rv.date = rs[0][0].slice(0, 10)
+
+    rv.hAbv = rs[0][4]
+    rv.hScore = rs[0][22]
+    rv.hW = +rs[0][7].split('-')[0]
+    rv.hL = +rs[0][7].split('-')[1]
+
+    rv.vAbv = rs[1][4]
+    rv.vScore = rs[1][22]
+    rv.vW = +rs[1][7].split('-')[0]
+    rv.vL = +rs[1][7].split('-')[1]
+
+    if (rv.hScore > rv.vScore){
+      rv.hW--
+      rv.vL--
+    } else{
+      rv.hL--
+      rv.vW--
+    }
+
+    rv.tW = rv.hW + rv.hW
+    rv.tL = rv.hL + rv.hL
+    
+    return rv
+  })
+  .filter(d => d)
 
 
-var rs = res.resultSets[5].rowSet
-var rv = {}
-rv.date = rs[0][0].slice(0, 10)
-
-rv.hAbv = rs[0][4]
-rv.hScore = rs[0][22]
-rv.hW = +rs[0][7].split('-')[0]
-rv.hL = +rs[0][7].split('-')[1]
-
-rv.vAbv = rs[1][4]
-rv.vScore = rs[1][22]
-rv.vW = +rs[1][7].split('-')[0]
-rv.vL = +rs[1][7].split('-')[1]
-
-if (rv.hScore > rv.vScore){
-  rv.hW--
-  rv.vL--
-} else{
-  rv.hL--
-  rv.vW--
-}
-
-rv.tW = rv.hW + rv.hW
-rv.tL = rv.hL + rv.hL
+fs.writeFileSync(__dirname + '/games.json', d3.csv.format(games))
