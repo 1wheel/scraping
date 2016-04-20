2628be285ed3855a448ba621cac0e6293b5a19dc
Adam Pearce <1wheel@gmail.com>
2016-04-20 18:44:10 -0400

    isIn not storing state???

--- mvp-minutes/parseGames.js
+++ mvp-minutes/parseGames.js
@@ -9,11 +9,14 @@ var glob = require("glob")
 
 var q = queue(1)
 var subs = []
+var blocks = []
 
 glob.sync(__dirname + "/raw-games/*.html").forEach(scrape)
 
 
 function scrape(file, gameIndex){
+  if (gameIndex) return
+
   var html = fs.readFileSync(file, 'utf-8')
 
   var isPlayoff = ~html.indexOf('Game 1')
@@ -22,22 +25,58 @@ function scrape(file, gameIndex){
 
   var count = 0
   var qtr = 1
+  var tableStarted = false
 
-  $('tr').each(function(){
+  $('tr').each(function(i){
+    //ignore cells until playbyplay starts
+    tableStarted = tableStarted || $(this).attr('id') == 'q1'
+    if (!tableStarted) return
+
     var str = $(this).text()
 
-    if ( ~str.indexOf('Back to Top')) qtr++
+    var inLastQ = false   //was curry playing at the end of last quater?
+    var inThisQ = false   //did curry play this quater?
+    var isIn = false     //was curry playing last time log mentioned him?
+
+    if (~str.indexOf('Back to Top')){
+      //if curry had no events for the entire quater, sub 
+      if (!inThisQ && isIn){
+        subs.push({gameIndex, qtr, time: '00:00', isIn: false, str})
+        isIn = false
+      }
+
+      qtr++
+      inLastQ = isIn
+      inThisQ = false
+
+    }
+      console.log(isIn)
 
     var playerI = str.indexOf('S. Curry')
     var entersI = str.indexOf('enters the game')
-    if (!~entersI || !~playerI) return
-    // $(this.parent).text().split('\n')[1]
-    
+    if (!~playerI) return
+
     var time = $(this).text().split('\n')[1]
-    var slug = _.last(file.split('/')).replace('.html', '')
-    var isIn = playerI < entersI
-    console.log(gameIndex, qtr, time)
-    subs.push({gameIndex, qtr, time, isIn, str})
+
+    //is this a sub log item?
+    if (!~entersI){
+      //insert a sub in at start of quater if curry made a play while marked out
+      if (!isIn && !inLastQ){
+        // if ()
+
+        isIn = true
+        subs.push({gameIndex, qtr, time: '12:00', isIn, str})
+        inThisQ = true
+      }
+
+    } else{
+      isIn = playerI < entersI
+      subs.push({gameIndex, qtr, time, isIn, str})
+      inThisQ = true
+      console.log(isIn)
+      
+    }
+
   })
 }
 
--- mvp-minutes/public/script.js
+++ mvp-minutes/public/script.js
@@ -8,16 +8,44 @@ d3.csv('subs.csv', function(res){
     d.isIn = d.isIn == 'true'
   })
 
-  c = d3.conventions({width: 100})
+  byGame = d3.nest().key(ƒ('gameIndex')).entries(subs)
 
-  c.x.domain([0, 48])
-  c.y.domain([0, 81])
+  byGame.forEach(function(game){
+    game.playBlocks = []
+    isPlaying = true
 
-  c.svg.dataAppend(subs, 'circle')
-      .attr('cx', ƒ('min', c.x))
-      .attr('cy', ƒ('gameIndex', c.y))
-      .attr('fill', function(d){ return d.isIn ? 'green' : 'red'})
-      .attr('r', 2)
+    game.values.forEach(function(d){
+
+    })
+  })
+
+  !(function(){
+    c = d3.conventions({width: 100})
+
+    c.x.domain([0, 48])
+    c.y.domain([81, 0])
+
+    c.svg.dataAppend(subs, 'circle')
+        .attr('cx', ƒ('min', c.x))
+        .attr('cy', ƒ('gameIndex', c.y))
+        .attr('fill', function(d){ return d.isIn ? 'steelblue' : 'red'})
+        .attr('r', function(d){ return d.time == '12:00' || d.time == '00.00' ? 1 : 3 })    
+        .call(d3.attachTooltip)
+  })()
+
+
+  !(function(){
+    c = d3.conventions({width: 1000})
+
+    c.x.domain([0, 48])
+    c.y.domain([81, 0])
+
+    c.svg.dataAppend(subs, 'circle')
+        .attr('cx', ƒ('min', c.x))
+        .attr('cy', ƒ('gameIndex', c.y))
+        .attr('fill', function(d){ return d.isIn ? 'green' : 'red'})
+        .attr('r', 2)    
+  })()
 
 
 })
