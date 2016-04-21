72e4bc63511c026fd2a0240dd54356c22b002a6d
Adam Pearce <1wheel@gmail.com>
2016-04-21 16:55:24 -0400

    dls player gamelogs

---
+++ mvp-minutes/downloadPlayers.js
@@ -0,0 +1,27 @@
+var fs = require('fs')
+var d3 = require('d3')
+var request = require('request')
+var cheerio = require('cheerio')
+var queue = require('queue-async')
+var _ = require('underscore')
+var glob = require("glob")
+
+var q = queue(1)
+
+var players = d3.csv.parse(fs.readFileSync(__dirname + '/players.csv', 'utf-8'))
+
+players.forEach(function(player){
+  if (!player.slug) return
+  
+  //http://www.basketball-reference.com/players/c/curryst01/gamelog/2016/
+  var url = 'http://www.basketball-reference.com/players/' + player.slug[0] + '/' + player.slug + '/gamelog/2016'
+
+  q.defer(function(cb){
+    request(url, function(error, response, html){
+      var playerDir = __dirname + '/raw/' + player.year + '-' + player.slug
+      if (!fs.existsSync(playerDir)) fs.mkdirSync(playerDir)
+      fs.writeFileSync(playerDir + '/gamelog.html', html)
+      setTimeout(cb, 1000)
+    })
+  })
+})
