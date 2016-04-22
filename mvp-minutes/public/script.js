a3a00a109dc33902f55dc263e737330d52ad7850
Adam Pearce <1wheel@gmail.com>
2016-04-22 11:44:04 -0400

    start sm percent played

--- mvp-minutes/public/script.js
+++ mvp-minutes/public/script.js
@@ -4,6 +4,8 @@ d3.json('players.json', function(res){
   players = res
 
   players.forEach(function(player){
+    if (!player.subs) return
+
     player.subs.forEach(function(d){
       d.min = -+d.time.split(':')[0] - +d.time.split(':')[1]/60 + qtrOffset[d.qtr]
       d.isIn = d.isIn == 'true'
@@ -27,6 +29,23 @@ d3.json('players.json', function(res){
       game.correctOrder = game.playBlocks.every(ƒ('correctOrder'))
     })
 
+    //calculate minute played blocks
+    player.gamesPlayed = player.byGame.length
+    player.minutesPlayed = d3.range(0, 48).map(function(d){ return 0 })
+
+    _.flatten(player.byGame.map(ƒ('playBlocks'))).forEach(function(d){
+      var firstMin = Math.ceil(d.start.min)
+      var lastMin  = Math.floor(d.end.min)
+      d3.range(firstMin, lastMin).forEach(function(min){
+        player.minutesPlayed[min]++
+        player.minutesPlayed[lastMin] += d.end.min - lastMin
+        player.minutesPlayed[Math.floor(d.start.min)] += firstMin - d.start.min
+      })
+    })
+
+    player.minutesPlayed = player.minutesPlayed.slice(0, 47)
+
+
     var playedGames = {}
     player.byGame.forEach(function(d){ playedGames[d.key] = true })
     d3.range(1, 83).forEach(function(d){
@@ -34,49 +53,83 @@ d3.json('players.json', function(res){
     })
 
 
-    d3.select('body').append('span').text(player.year + ' - ' + player.fullName)
-    c = d3.conventions({width: 82*3.5, height: 140})
-    c.svg.datum(player)
 
-    c.x.domain([0, 83])
-    c.y.domain([48, 0])
 
-    var gameSel = c.svg.dataAppend(player.byGame, 'g.game')
-        .translate(function(d){ return [c.x(d.key), 0] })
-        .style('opacity', function(d){
-          return d.correctOrder ? 1 : 1
-        })
-    
-    gameSel.append('path')
-        .attr('d', ['M0,', c.y(0), 'V', c.y(48)].join(' '))
-        .style('stroke', 'red')
-        .style('stroke-width', 2)
-        .style('opacity', .15)
-        
-    gameSel.filter(ƒ('values', 'length')).append('path')
-        .attr('d', ['M0,', c.y(42), 'V', c.y(48)].join(' '))
-        .style('stroke', 'red')
-        .style('stroke-width', 2)
+    //sm
+    !(function(){
+      var sel = d3.select('#sm-graph').append('div').style({display: 'inline-block', 'margin-bottom': '10px'})
 
-    gameSel.dataAppend(ƒ('playBlocks'), 'path.block')
-        .attr('d', function(d){
-          return ['M0,', c.y(d.start.min), 'V', c.y(d.end.min)].join(' ') })
-        .style('stroke', '#666')
-        .style('stroke-width', 2)
+      c = d3.conventions({width: 82*2.5, height: 70, parentSel: sel})
+      sel.append('h3').text(player.year + ' - ' + player.fullName)
+          .style({'text-align': 'center', 'margin-top': '-15px'})
+      c.svg.datum(player)
 
-    c.xAxis.orient('top').tickValues([41, 82])
-    c.yAxis.tickValues([12, 24, 36, 48]).tickSize(c.width)
-    c.drawAxis()
-    c.svg.select('.x').translate([0, 0])
-    c.svg.select('.y').translate([c.width, 0])
+      c.x.domain([0, 83])
+      c.y.domain([48, 0])
 
+      var gameSel = c.svg.dataAppend(player.byGame, 'g.game')
+          .translate(function(d){ return [c.x(d.key), 0] })
+          .style('opacity', function(d){
+            return d.correctOrder ? 1 : 1
+          })
+      
+      gameSel.append('path')
+          .attr('d', ['M0,', c.y(0), 'V', c.y(48)].join(' '))
+          .style('stroke', 'red')
+          .style('stroke-width', 1.5)
+          .style('opacity', .15)
+          
+      gameSel.filter(ƒ('values', 'length')).append('path')
+          .attr('d', ['M0,', c.y(42), 'V', c.y(48)].join(' '))
+          .style('stroke', 'red')
+          .style('stroke-width', 1.5)
+
+      gameSel.dataAppend(ƒ('playBlocks'), 'path.block')
+          .attr('d', function(d){
+            return ['M0,', c.y(d.start.min), 'V', c.y(d.end.min)].join(' ') })
+          .style('stroke', '#666')
+          .style('stroke-width', 1.5)
+
+      c.xAxis.orient('top').tickValues([41, 82])
+      c.yAxis.tickValues([12, 24, 36, 48]).tickSize(c.width)
+      c.drawAxis()
+      c.svg.select('.x').translate([0, 0])
+      c.svg.select('.y').translate([c.width, 0])
+    })()
+
+    //play time
+    !(function(){
+      var sel = d3.select('#sm-percent').append('div')
+          .style({display: 'inline-block', 'margin-bottom': '10px'})
+
+      c = d3.conventions({width: 82*2.5, height: 70, parentSel: sel})
+      sel.append('h3').text(player.year + ' - ' + player.fullName)
+          .style({'text-align': 'center', 'margin-top': '-15px'})
+      c.svg.datum(player)
+
+      c.x.domain([0, 48])
+      c.y.domain([0, 1])
+
+      c.yAxis.ticks(3).tickFormat(d3.format('%'))
+      c.drawAxis()
+
+      var area = d3.svg.area()
+          .x(function(d, i){ return c.x(i) })
+          .y0(function(d){ return d/player.gamesPlayed })
+          .y1(c.height)
+
+      c.svg.append('path')
+          .attr('d', area(player.minutesPlayed))
+
+
+
+    })()
 
   })
 })
 
 
 d3.csv('subs.csv', function(res){
-  return;
   subs = res
 
   subs.forEach(function(d){
@@ -123,9 +176,10 @@ d3.csv('subs.csv', function(res){
         .call(d3.attachTooltip)
   })//()
 
+  var sel = d3.select('#curry-debug').append('div')
   !(function(){
-    d3.select('body').append('h1').text('small multiple prototype')
-    c = d3.conventions({width: 82*3.5, height: 140})
+    sel.append('h1').text('small multiple prototype')
+    c = d3.conventions({width: 82*3.5, height: 140, parentSel: sel})
 
     c.x.domain([0, 83])
     c.y.domain([48, 0])
@@ -163,8 +217,8 @@ d3.csv('subs.csv', function(res){
 
 
   !(function(){
-    d3.select('body').append('h1').text('debug playing blocks')
-    c = d3.conventions({width: 400})
+    sel.append('h1').text('debug playing blocks')
+    c = d3.conventions({width: 400, parentSel: sel})
 
     c.x.domain([0, 48])
     c.y.domain([81, 0])
@@ -192,8 +246,4 @@ d3.csv('subs.csv', function(res){
     d3.select('body').append('h1')
         .text('bad order: ' +  d3.sum(byGame, function(d){ return !d.correctOrder }))
   })()
-
-
-
-
 })
