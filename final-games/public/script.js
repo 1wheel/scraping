2d89e603d02e48251e2308b44842fb76653fb66d
Adam Pearce <1wheel@gmail.com>
2016-05-19 01:22:10 -0400

    sizes by points

--- final-games/public/script.js
+++ final-games/public/script.js
@@ -9,11 +9,11 @@ d3.csv('players.csv', function(res){
     d.series = d3.nest().key(ƒ('year')).entries(d.values)
   
     d.streaks = []
-    var curStreak = [d.series[0].key]
-    d.series.map(ƒ('key')).forEach(function(year, i){
+    var curStreak = [d.series[0]]
+    d.series.forEach(function(year, i){
       if (!i) return
 
-      if (_.last(curStreak) == year - 1){
+      if (_.last(curStreak).key == year.key - 1){
         curStreak.push(year)
       } else{
         d.streaks.push(curStreak)
@@ -21,15 +21,15 @@ d3.csv('players.csv', function(res){
       }
     })
     d.streaks.push(curStreak)
-
-    d.maxStreak = d3.max(d.streaks, ƒ('length'))
-    d.streaks.forEach(function(s){
-      if (s.length == d.maxStreak) d.maxStreakStart = s[0]
-    })
-
+    d.maxStreak = _.max(d.streaks, ƒ('length'))
+    d.maxStreakStart = d.maxStreak[0].key
+    d.maxStreakPts = d3.mean(_.flatten(d.maxStreak.map(ƒ('values'))), ƒ('pts'))
   })
 
+  var topPlayers = byPlayer.filter(function(d){ return d.series.length > 2 })
 
+
+  d3.select('body').append('h2').text('distribution of # finals series a player played in')
   !(function(){
     var c = d3.conventions({width: 900})
 
@@ -45,8 +45,8 @@ d3.csv('players.csv', function(res){
         .call(d3.attachTooltip)
   })()
 
+  d3.select('body').append('h2').text('distribution of # finals series players w/ at least 3 played in')
   !(function(){
-    var topPlayers = byPlayer.filter(function(d){ return d.series.length > 2 })
     var c = d3.conventions({width: 900})
 
     c.x.domain([0, topPlayers.length - 1])
@@ -61,31 +61,29 @@ d3.csv('players.csv', function(res){
         .call(d3.attachTooltip)
   })()
 
+  d3.select('body').append('h2').text('most consecutive finals v total finals')
   !(function(){
-    //tktk - cluster?
-    var topPlayers = byPlayer.filter(function(d){ return d.series.length > 2 })
     var c = d3.conventions({width: 900})
 
-    c.x.domain([1, d3.max(topPlayers, ƒ('maxStreak'))])
+    c.x.domain([1, d3.max(topPlayers, ƒ('maxStreak', 'length'))])
     c.y.domain([0, d3.max(topPlayers, ƒ('series', 'length'))])
 
     c.drawAxis()
 
     c.svg.dataAppend(_.sortBy(topPlayers, ƒ('series', 'length')), 'circle')
         .attr('r', 2)
-        .attr('cx', ƒ('maxStreak', c.x))
+        .attr('cx', ƒ('maxStreak', 'length', c.x))
         .attr('cy', ƒ('series', 'length', c.y))
         .call(d3.attachTooltip)
         .translate(function(){ return [Math.random()*6, Math.random()*6] })
   })()
 
+  d3.select('body').append('h2').text('# of consecutive finals overtime')
   !(function(){
-    //tktk - cluster?
-    var topPlayers = byPlayer.filter(function(d){ return d.series.length > 2 })
     var c = d3.conventions({width: 900})
 
     c.x.domain([1950, 2015])
-    c.y.domain([1, d3.max(topPlayers, ƒ('maxStreak'))])
+    c.y.domain([1, d3.max(topPlayers, ƒ('maxStreak', 'length'))])
 
     c.xAxis.tickFormat(function(d){ return d })
     c.drawAxis()
@@ -93,33 +91,35 @@ d3.csv('players.csv', function(res){
     c.svg.dataAppend(topPlayers, 'circle')
         .attr('r', 3)
         .attr('cx', ƒ('maxStreakStart', c.x))
-        .attr('cy', ƒ('maxStreak', c.y))
+        .attr('cy', ƒ('maxStreak', 'length', c.y))
         .call(d3.attachTooltip)
         .translate(function(){ return [Math.random()*6, Math.random()*6] })
         .styles({stroke: 'black', fill: 'none'})
   })()
 
+  d3.select('body').append('h2').text('# of consecutive finals overtime - sized by avg points scored')
   !(function(){
-    var topPlayers = byPlayer.filter(function(d){ return d.series.length > 2 })
     var c = d3.conventions({width: 900})
 
     c.x.domain([1950, 2015])
-    c.y.domain([1, d3.max(topPlayers, ƒ('maxStreak'))])
+    c.y.domain([1, d3.max(topPlayers, ƒ('maxStreak', 'length'))])
+
+    var r = d3.scaleSqrt().domain([0, 40]).range([.5, 10])
 
     c.xAxis.tickFormat(function(d){ return d })
     c.drawAxis()
 
     var simulation = d3.forceSimulation(topPlayers)
         .force("x", d3.forceX(ƒ('maxStreakStart', c.x)))//.strength(1))
-        .force("y", d3.forceY(ƒ('maxStreak', c.y)))
-        .force("collide", d3.forceCollide(4))
+        .force("y", d3.forceY(ƒ('maxStreak', 'length', c.y)))
+        .force("collide", d3.forceCollide(ƒ('maxStreakPts', r, add(.5))))
         .stop()
 
     for (var i = 0; i < 120; ++i) simulation.tick()
 
 
     c.svg.dataAppend(topPlayers, 'circle')
-        .attr('r', 3)
+        .attr('r', ƒ('maxStreakPts', r))
         .call(d3.attachTooltip)
         .translate(function(d){ return [d.x, d.y] })
 
@@ -128,3 +128,5 @@ d3.csv('players.csv', function(res){
 
   //table(_.sortBy(byPlayer, ƒ('values', 'length')))
 })
+
+function add(num){ return function(d){ return d + num }}
